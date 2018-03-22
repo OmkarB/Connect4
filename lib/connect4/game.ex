4f8dff37fe3a80da2b7330162cb014df5b3cace6
Omkar Bhat <omkar1496@gmail.com>
2018-03-22 01:22:51 -0400

    Make moves

--- lib/connect4/game.ex
+++ lib/connect4/game.ex
@@ -4,12 +4,26 @@ defmodule Connect4.Game do
       %{
         board: create_board(),
         role: 'red',
-        game_over: False
+        winner: nil
       }
     end
 
     def create_board() do
       row = [nil, nil, nil, nil, nil, nil, nil]
       board = for i <- 0..5, do: row
+    end
+
+    def find_last_index(column) do
+      7 - length(Enum.filter(column, fn(coin) ->
+        match?({nil, _}, coin)
+      end))
+    end
+
+    def move(game, role, columnIdx) do
+      case Enum.at(game.board, columnIdx) do
+        column ->
+          row = find_last_index(column)
+          List.replace_at(column, row, role)
+      end
     end
 end
