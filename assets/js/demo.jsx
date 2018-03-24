b64d2ca525fae31cb5537e9b840e6437649012c1
Vignesh Mohankumar <vmohankumar@driftt.com>
2018-03-24 14:27:08 -0400

    add game

--- assets/js/demo.jsx
+++ assets/js/demo.jsx
@@ -1,23 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import { Button } from 'reactstrap'
-import _ from 'lodash/fp'
+import cx from 'classnames'
+import { findLastIndex } from 'ramda'
 
 export default function run_demo(root, channel) {
   ReactDOM.render(<Demo channel={channel}/>, root)
 }
 
-class Demo extends React.Component {
+const colors = {
+  RED: 'RED',
+  YELLOW: 'YELLOW',
+}
+
+const DEFAULT_GRID = [
+  [undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+  [undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+  [undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+  [undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+  [undefined, colors.YELLOW, undefined, undefined, undefined, undefined, undefined],
+  [undefined, colors.YELLOW, undefined, undefined, colors.RED, undefined, undefined],
+]
+
+class Demo extends Component {
   constructor(props) {
     super(props)
     this.channel = props.channel
     this.state = {
       loaded: false,
-      clicks: undefined,
-      letters: undefined,
-      completed: undefined,
-      checkIndex: undefined,
-      confirmIndex: undefined,
+      turn: colors.RED,
+      grid: DEFAULT_GRID,
+      winner: undefined,
+      role: colors.RED,
     }
     this.channel
       .join()
@@ -25,70 +38,47 @@ class Demo extends React.Component {
         game['loaded'] = true
         this.setState(game)
       })
-    this.toggle = this.toggle.bind(this)
-    this.afterToggle = this.afterToggle.bind(this)
-    this.reset = this.reset.bind(this)
+    this.selectColumn = this.selectColumn.bind(this)
   }
 
-  reset() {
-    this.channel
-      .push('reset')
-      .receive('ok', ({ game }) => this.setState(game))
-  }
-
-  afterToggle() {
-    const { confirmIndex, checkIndex, letters, completed } = this.state
-    if (!_.isNil(confirmIndex) && !_.isNil(checkIndex)) {
-      const interval = setInterval(() => {
-        this.channel
-          .push('clear_indices')
-          .receive('ok', ({ game }) => this.setState(game))
-      }, 1000)
-      setTimeout(() => clearInterval(interval), 1000)
+  selectColumn(columnIndex) {
+    return () => {
+      const { grid, turn } = this.state
+      const rowIndex = findLastIndex(row => !row[columnIndex], grid)
+      grid[rowIndex][columnIndex] = turn
+      this.setState({ turn: turn === colors.RED ? colors.YELLOW : colors.RED })
     }
   }
 
-  toggle(i) {
-    const { clicks, confirmIndex, checkIndex } = this.state
-    this.channel
-      .push('toggle', { index: i })
-      .receive('ok', ({ game }) => this.setState(game, this.afterToggle))
-  }
-
   render() {
-    const { letters, clicks, completed, checkIndex, confirmIndex, loaded } = this.state
+    const { loaded, grid, winner, role, turn } = this.state
     if (!loaded) return false
     return (
-      <div className="memory-game">
-        <div>clicks {clicks}</div>
-        <Button onClick={this.reset}>Reset</Button>
-        <div className="memory-game-grid">
-          {this.state.letters.map((letter, i) =>
-            <Side
-              letter={letter}
-              key={i}
-              i={i}
-              visible={checkIndex === i || confirmIndex === i}
-              completed={completed.includes(letter)}
-              toggle={this.toggle}
-            />
+      <div>
+        <div className="overview">
+          {!!winner &&
+            `Game over, ${role === winner ? 'you win' : 'the opponent wins'}!`
+          }
+          {!winner && (turn === role ? 'Your turn' : "Opponent's turn")}
+        </div>
+        <div className="grid">
+          {grid.map((row, rowIndex) =>
+            <div className="row" key={`row-${rowIndex}`}>
+              {row.map((coin, columnIndex) =>
+                <div
+                  key={`coin-${columnIndex}-${rowIndex}`}
+                  onClick={rowIndex === 0 && !coin ? this.selectColumn(columnIndex) : undefined}
+                  className={cx(
+                    'coin',
+                    { 'is-selectable': rowIndex === 0 && !coin },
+                    { 'is-yellow': coin === colors.YELLOW, 'is-red': coin === colors.RED }
+                  )}
+                />
+              )}
+            </div>
           )}
         </div>
       </div>
     )
   }
-}
-
-function Side(props) {
-  const { visible, completed, i, letter, toggle } = props
-  return (
-    <div className="memory-game-tile">
-      {visible
-        ? letter
-        : completed
-          ? 'complete'
-          : <Button onClick={() => toggle(i)}>Click Me</Button>
-      }
-    </div>
-  )
 }
