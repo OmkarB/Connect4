8d1e12acacb9286e7a2830d5fcdd9fc7b8d0731a
Vignesh Mohankumar <vmohankumar@driftt.com>
2018-03-24 14:19:06 -0400

    more

--- lib/connect4_web/channels/games_channel.ex
+++ lib/connect4_web/channels/games_channel.ex
@@ -1,28 +1,13 @@
-defmodule Connect4.GamesChannel do
-	use Connect4Web, :channel
+defmodule Connect4Web.GamesChannel do
+  use Connect4Web, :channel
 
-	alias Connect4.Game
+  alias Connect4.Game
 
-	def join("games:" <> name, payload, socket) do
-		if authorized?(payload) do
-			game = Connect4.GameBackup.load(name) || Game.new()
-			socket = socket
-			|> assign(:game, game)
-			|> assign(:name, game)
-			{:ok, %{"join" => name, "game" => Game.client_view(game)}, socket}
-		else
-			{:error, %{reason: "unauthorized"}}
-		end
-	end
-
-	def handle_in("move", %{"role" => role, "column" => column}, socket) do
-		game = Game.move(socket.assigns[:game], role, column)
-		Connect4.GameBackup.save(socket.assign[:name], game)
-		socket = assign(socket, :game, game)
-		{:reply, {:ok, %{"game" => game}}, socket}
-	end
-
-	defp authorized?(_payload) do
-		true
-	end
+  def join("games:" <> name, payload, socket) do
+    game = Connect4.GameBackup.load(name) || Game.new()
+    socket = socket
+    |> assign(:game, game)
+    |> assign(:name, name)
+    {:ok, %{"join" => name, "game" => game}, socket}
+  end
 end
